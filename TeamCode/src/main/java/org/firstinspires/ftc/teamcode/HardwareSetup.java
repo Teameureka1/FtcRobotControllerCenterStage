2ae24469c18a1d0a56d38f129da21d6cf4476d13
Chester <109835029+narlyx@users.noreply.github.com>
2024-03-02 09:48:53 -0700

    Hardened Autonomous stop force modifier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSetup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSetup.java
@@ -247,7 +247,7 @@ public class HardwareSetup
                 .setMaxSpeed(0.8)
                 .setStartSpeed(0.4)
                 .setSpeedModifier(0.04)
-                .setStopForceSpeed(0.1)
+                .setStopForceSpeed(0.2)
 
                 .setCorrectionOverpowerDistance(5)
                 .setDistanceBuffer(1)
