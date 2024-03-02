dad82f0c1a4efe739def4b7252573624892ecc66
Chester <109835029+narlyx@users.noreply.github.com>
2024-03-02 10:28:26 -0700

    Once more adjusted stop force speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSetup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSetup.java
@@ -247,7 +247,7 @@ public class HardwareSetup
                 .setMaxSpeed(0.8)
                 .setStartSpeed(0.4)
                 .setSpeedModifier(0.04)
-                .setStopForceSpeed(0.3)
+                .setStopForceSpeed(0.25)
 
                 .setCorrectionOverpowerDistance(5)
                 .setDistanceBuffer(1)
